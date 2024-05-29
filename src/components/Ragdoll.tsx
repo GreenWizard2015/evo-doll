19ef0a63ae9ede4bf7bc148b964fe012fbdacf11
GreenWizard2015 <forwork.anton@gmail.com>
2024-05-29 16:43:51 +0000

    state, refs, etc

--- src/components/Ragdoll.tsx
+++ src/components/Ragdoll.tsx
@@ -1,47 +1,59 @@
-import { createContext, useContext, useRef } from 'react';
+import React, { createContext, useContext, useRef, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useBox, useConeTwistConstraint } from '@react-three/cannon';
 import { createRagdoll } from '../helpers/createRagdoll';
 import { useDragConstraint } from '../helpers/Drag';
 import { Block } from '../helpers/Block';
 
-const { shapes, joints } = createRagdoll(.5, Math.PI / 16, Math.PI / 16, 0);
+const { shapes, joints } = createRagdoll(1.5, Math.PI / 16, Math.PI / 16, 0);
 const context = createContext();
 
-const BodyPart = ({ config, children, render, name, ...props }) => {
+const BodyPart = ({ setApi, config, children, render, name, ...props }: 
+   { setApi?: any, config?: any, children?: any, render?: any, name: string }) => {
   const { color, args, mass, position } = shapes[name];
   const parent = useContext(context);
-  const [ref] = useBox(() => ({ mass, args, position, linearDamping: 0.99, ...props }))
-  useConeTwistConstraint(ref, parent, config)
-  const bind = useDragConstraint(ref)
+  const [boxRef, api] = useBox(() => ({ mass, args, position, ...props }));
+  useEffect(() => {
+    if (setApi) setApi(api, boxRef);
+  }, [api, setApi, boxRef]);
+  useConeTwistConstraint(boxRef, parent, config);
+  const bind = useDragConstraint(boxRef);
   return (
-    <context.Provider value={ref}>
-      <Block castShadow receiveShadow ref={ref} {...props} {...bind} scale={args} name={name} color={color}>
+    <context.Provider value={boxRef}>
+      <Block castShadow receiveShadow ref={boxRef} {...props} {...bind} scale={args} name={name} color={color}>
         {render}
       </Block>
       {children}
     </context.Provider>
   );
-}
+};
 
-export function Ragdoll(props) {
+export function Ragdoll({ onState, props }: { onState: (state: any) => void, props: any}) {
+  const state = useRef({ });
+  useEffect(() => onState && onState(state), [onState, state]);
+
+  function bind(api, ref) {
+    const name = ref.current.name;
+    state.current[name] = { api, ref };
+  }
+
   return (
-    <BodyPart name="upperBody" {...props}>
-      <BodyPart {...props} name="head" config={joints['neckJoint']} />
-        <BodyPart {...props} name="upperLeftArm" config={joints['leftShoulder']}>
-          <BodyPart {...props} name="lowerLeftArm" config={joints['leftElbowJoint']} />
+    <BodyPart name="upperBody" setApi={bind} {...props}>
+      <BodyPart {...props} name="head" setApi={bind} config={joints['neckJoint']} />
+      <BodyPart {...props} name="upperLeftArm" setApi={bind} config={joints['leftShoulder']}>
+        <BodyPart {...props} name="lowerLeftArm" setApi={bind} config={joints['leftElbowJoint']} />
+      </BodyPart>
+      <BodyPart {...props} name="upperRightArm" setApi={bind} config={joints['rightShoulder']}>
+        <BodyPart {...props} name="lowerRightArm" setApi={bind} config={joints['rightElbowJoint']} />
+      </BodyPart>
+      <BodyPart {...props} name="pelvis" setApi={bind} config={joints['spineJoint']}>
+        <BodyPart {...props} name="upperLeftLeg" setApi={bind} config={joints['leftHipJoint']}>
+          <BodyPart {...props} name="lowerLeftLeg" setApi={bind} config={joints['leftKneeJoint']} />
         </BodyPart>
-        <BodyPart {...props} name="upperRightArm" config={joints['rightShoulder']}>
-          <BodyPart {...props} name="lowerRightArm" config={joints['rightElbowJoint']} />
-        </BodyPart>
-        <BodyPart {...props} name="pelvis" config={joints['spineJoint']}>
-          <BodyPart {...props} name="upperLeftLeg" config={joints['leftHipJoint']}>
-            <BodyPart {...props} name="lowerLeftLeg" config={joints['leftKneeJoint']} />
-          </BodyPart>
-          <BodyPart {...props} name="upperRightLeg" config={joints['rightHipJoint']}>
-            <BodyPart {...props} name="lowerRightLeg" config={joints['rightKneeJoint']} />
-          </BodyPart>
+        <BodyPart {...props} name="upperRightLeg" setApi={bind} config={joints['rightHipJoint']}>
+          <BodyPart {...props} name="lowerRightLeg" setApi={bind} config={joints['rightKneeJoint']} />
         </BodyPart>
       </BodyPart>
+    </BodyPart>
   );
 }
