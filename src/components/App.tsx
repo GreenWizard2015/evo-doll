812a23fbcb948ee2d3030567cb52b0628118eb7d
GreenWizard2015 <forwork.anton@gmail.com>
2024-05-30 14:40:13 +0000

    score

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import { Physics } from '@react-three/cannon';
 import { RAGDOLL_PARTS, Ragdoll, encodeObservation } from './Ragdoll';
-import { OrbitControls } from '@react-three/drei';
+import { Html, OrbitControls } from '@react-three/drei';
 import Scene from './Scene';
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
@@ -32,6 +32,7 @@ function Debug({playerA, playerB}) {
   const modelA = useRef(createModel({inputSize: 240, outputSize: RAGDOLL_PARTS.length}));
   const modelB = useRef(createModel({inputSize: 240, outputSize: RAGDOLL_PARTS.length}));
   const raycaster = useRef(new THREE.Raycaster());
+  const tick = useRef(0);
 
   useFrame(({ scene }) => {    
     const process = (player, model) => {
@@ -55,6 +56,9 @@ function Debug({playerA, playerB}) {
       }
     };
 
+    if (tick.current > 25) return;
+    tick.current += 1;
+    
     process(playerA.current, modelA.current);
     process(playerB.current, modelB.current);
   });
@@ -65,6 +69,7 @@ function Debug({playerA, playerB}) {
 const App: React.FC = () => {
   const playerA = useRef<any>(null);
   const playerB = useRef<any>(null);
+  const [scores, setScores] = React.useState({ playerA: 0, playerB: 0 });
 
   function bindPlayerA(ref) {
     playerA.current = ref.current;
@@ -75,23 +80,63 @@ const App: React.FC = () => {
   }
 
   function onCollide(e: CollisionEvent) {
-    console.log('collide', e);
+    if (!playerA.current || !playerB.current) return;
+    if (!playerA.current['head'] || !playerB.current['head']) return;
+    const { body, target, contact: { impactVelocity } } = e;
+    if (impactVelocity < 0) return;
+    const playerAGroup = playerA.current['head'].ref.current.collisionFilterGroup;
+    const playerBGroup = playerB.current['head'].ref.current.collisionFilterGroup;
+    const score = Math.max(0, impactVelocity);
+    const scoresNew = { ...scores };
+    // penalize the player that is hit
+    if(target.collisionFilterGroup === playerAGroup) {
+      scoresNew.playerA -= score;
+      console.log('playerA hit', e);
+      
+    }
+    if(target.collisionFilterGroup === playerBGroup) {
+      scoresNew.playerB -= score;
+    }
+    // reward the player that hits
+    if(body.collisionFilterGroup === playerAGroup) {
+      scoresNew.playerA += score;
+      console.log('playerB hit by playerA', e);
+    }
+    if(body.collisionFilterGroup === playerBGroup) {
+      scoresNew.playerB += score;
+    }
+    
+    setScores(scoresNew); // update the scores
   }
 
-  return (
-    <Canvas id='canvas' style={{ position: 'absolute' }}>
-      <CustomCamera />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Physics>
-        <Scene />
+  const playerProps = {
+    onCollide,
+    // moving only on x and z axis
+    linearFactor: [1, 1, 0],
+    angularFactor: [0, 1, 0],
+  };
 
-        <Ragdoll onState={bindPlayerA} props={{ position: [-2, 0, 0], onCollide }} />
-        <Ragdoll onState={bindPlayerB} props={{ position: [2, 0, 0], onCollide }} />
-      </Physics>
-      <OrbitControls />
-      <Debug playerA={playerA} playerB={playerB} />
-    </Canvas>
+  return (
+    <>
+      <Canvas id='canvas' style={{ position: 'absolute' }}>
+        <CustomCamera />
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Physics>
+          <Scene />
+
+          <Ragdoll onState={bindPlayerA} props={{ position: [-2, 0, 0], ...playerProps }} />
+          <Ragdoll onState={bindPlayerB} props={{ position: [2, 0, 0], ...playerProps }} />
+        </Physics>
+        <OrbitControls />
+        <Debug playerA={playerA} playerB={playerB} />
+      </Canvas>
+
+      <div style={{ position: 'absolute', top: 0, left: 0, color: 'white', padding: '10px', background: 'rgba(0, 0, 0, 0.5)', borderRadius: '5px' }}>
+        <div>Player A: {scores.playerA}</div>
+        <div>Player B: {scores.playerB}</div>
+      </div>
+    </>
   );
 }
 
