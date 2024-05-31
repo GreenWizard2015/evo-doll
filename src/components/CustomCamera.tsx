66d6924854a477dfc77ed1a58eed413d04b5c64a
GreenWizard2015 <forwork.anton@gmail.com>
2024-05-31 12:43:19 +0000

    fix

--- src/components/CustomCamera.tsx
+++ src/components/CustomCamera.tsx
@@ -1,13 +1,12 @@
-import { PerspectiveCameraProps } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 
 function CustomCamera() {
-  const PerspectiveCamera = useRef<PerspectiveCameraProps>(null);
+  const PerspectiveCamera = useRef<THREE.PerspectiveCamera>(null);
 
   useEffect(() => {
     if (PerspectiveCamera.current) {
-      PerspectiveCamera.current.position = new THREE.Vector3(0, 5, 10);
+      PerspectiveCamera.current.position.set(0, 5, 10);
       PerspectiveCamera.current.fov = 95;
       PerspectiveCamera.current.near = 0.1;
       PerspectiveCamera.current.far = 1000;
