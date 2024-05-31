9e74272c303be62603fee1246109cfe09f9e4254
GreenWizard2015 <forwork.anton@gmail.com>
2024-05-31 13:17:08 +0000

    fix

--- src/helpers/Block.js
+++ src/helpers/Block.js
@@ -1,14 +1,14 @@
-import React, { forwardRef } from 'react';
+import { forwardRef } from 'react';
 import { RoundedBox } from '@react-three/drei';
 
 const defaultProps = {
   transparent: false,
   opacity: 1,
   color: 'white',
-  args: [1, 1, 1],
+  args: [1, 1, 1]
 };
 
-const Block = forwardRef(({ children, params }, ref) => {
+export const Block = forwardRef(({children, ...params}, ref) => {
   const { args, color, transparent, opacity, ...props } = { ...defaultProps, ...params };
   return (
     <RoundedBox args={args} receiveShadow castShadow ref={ref} {...props}>
@@ -17,8 +17,3 @@ const Block = forwardRef(({ children, params }, ref) => {
     </RoundedBox>
   );
 });
-
-Block.displayName = 'Block';
-
-export default Block;
-export { Block };
